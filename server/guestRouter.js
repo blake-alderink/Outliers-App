50d545cb8e70048061dfa7509a287c647cda6604
Blake Alderink <blake.alderink@gmail.com>
2023-06-11 00:05:59 -0700

    added redux toolkit and Create React App client side functionality

--- server/guestRouter.js
+++ server/guestRouter.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 
 
 const router = express.Router();
+router.use(express.json());
+router.use(cors())
 
 router.use('/', (req, res, next) => {
     console.log('router is working! continue forward')
     next();
 })
 
-router.get('/', (req, res) => {
-    res.json('this router is WORKING BABY')
+router.get('/', async (req, res) => {
+try {
+    
+    const favorites = await pool.query('SELECT * FROM favorites JOIN bets ON favorites.bet_ref=bets.bet_id WHERE favorites.user_ref = $1', [1]);
+    res.json(favorites.rows);
+    console.log('successful database query');
+
+} catch (error) {
+    console.log(error.message);
+}
+    //get favorites where user_id is equal to "1"
+
+    // res.json('this router is WORKING BABY')
 })
 
-router.use(express.json());
-router.use(cors())
+
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const guestRouter = require('./guestRouter');
+const pool = require('./db');
 
 const app = express();
 
 app.use(express.json());
 app.use('/guest', guestRouter)
 
-app.get('/', (req, res) => {
-    res.json("if this is showing up, then this is showing you that this is working")
+app.get('/', async (req, res) => {
+    try {
+        const users = await pool.query('SELECT * FROM users');
+        res.json(users.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+      // res.json("if this is showing up, then this is showing you that this is working")
 })
 
 app.listen(8000, () => {
